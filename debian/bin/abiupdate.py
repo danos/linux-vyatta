3981e8947e2d2bf6e47d9f419a886bd42c99e7dd
Ben Hutchings <ben@decadent.org.uk>
2018-01-12 02:33:17 +0000

    abiupdate.py: Add support for security mirrors
    (cherry picked from commit 30fe681bffa1bf38f1fdd650ee8f3631e0f57015)

--- debian/bin/abiupdate.py
+++ debian/bin/abiupdate.py
@@ -21,6 +21,7 @@ from debian_linux.debian import *
 default_url_base = "http://ftp.de.debian.org/debian/"
 default_url_base_incoming = "http://incoming.debian.org/"
 default_url_base_ports = "http://ftp.debian-ports.org/debian/"
+default_url_base_security = "http://security.debian.org/"
 
 
 class url_debian_flat(object):
@@ -44,6 +45,11 @@ class url_debian_ports_pool(url_debian_pool):
         if arch == 'all':
             return url_debian_pool.__call__(self, source, filename, arch)
         return self.base + "pool-" + arch + "/main/" + source[0] + "/" + source + "/" + filename
+
+
+class url_debian_security_pool(url_debian_pool):
+    def __call__(self, source, filename, arch):
+        return self.base + "pool/updates/main/" + source[0] + "/" + source + "/" + filename
 
 
 class Main(object):
@@ -187,9 +193,11 @@ if __name__ == '__main__':
     options.add_option("-i", "--incoming", action="store_true", dest="incoming")
     options.add_option("--incoming-config", action="store_true", dest="incoming_config")
     options.add_option("--ports", action="store_true", dest="ports")
+    options.add_option("--security", action="store_true", dest="security")
     options.add_option("-u", "--url-base", dest="url_base", default=default_url_base)
     options.add_option("--url-base-incoming", dest="url_base_incoming", default=default_url_base_incoming)
     options.add_option("--url-base-ports", dest="url_base_ports", default=default_url_base_ports)
+    options.add_option("--url-base-security", dest="url_base_security", default=default_url_base_security)
 
     opts, args = options.parse_args()
 
@@ -204,11 +212,14 @@ if __name__ == '__main__':
     url_base = url_debian_pool(opts.url_base)
     url_base_incoming = url_debian_flat(opts.url_base_incoming)
     url_base_ports = url_debian_ports_pool(opts.url_base_ports)
+    url_base_security = url_debian_security_pool(opts.url_base_security)
     if opts.incoming_config:
         url = url_config = url_base_incoming
     else:
         url_config = url_base
-        if opts.incoming:
+        if opts.security:
+            url = url_base_security
+        elif opts.incoming:
             url = url_base_incoming
         elif opts.ports:
             url = url_base_ports
